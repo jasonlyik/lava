3d726abf88568417c3ee27aa46e2923b9385dcdc
Jason Yik <jyik@g.harvard.edu>
2022-11-01 19:32:11 -0400

    airplane work lmao

--- benchmark/random.py
+++ benchmark/random.py
@@ -30,37 +30,82 @@ from lava.magma.core.run_configs import Loihi1SimCfg
 
 import numpy as np
 
-# TODO: will python find this, do i need an __init__.py
+# TODO: will python find this, do i need an __init__.py, or just dot ahead of spikegenerator
 from spikegenerator import SpikeGenerator, PySpikeGeneratorModel
 
+def random(N: int, d: float, num_steps: int):
+	load_time = 0 # TODO: add timing
+	sim_time = 0
+	monitor_time = 0
 
-# TODO: here is the baseline code, work on the evaluation framework surrounding like
-# making it run on different synaptic density, num neurons, adding timing stuff, and
-# avg over 5 runs
+	lif = LIF(shape=???, vth=, dv=, du=, bias_mant=, name="LIF_neurons")
+	dense = Dense(weights=???, name="dense")
 
-lif = LIF(shape=???, vth=, dv=, du=, bias_mant=, name="LIF_neurons")
-dense = Dense(weights=???, name="dense")
+	# recurrent connection
+	lif.s_out.connect(dense.s_in)
+	dense.a_out.connect(lif.a_in)
 
-# recurrent connection
-lif.s_out.connect(dense.s_in)
-dense.a_out.connect(lif.a_in)
+	monitor_lif = Monitor()
 
-monitor_lif = Monitor()
-num_steps = ???
+	monitor_lif.probe(lif.v, num_steps)
 
-monitor_lif.probe(lif.v, num_steps)
+	# TODO: this just needs spikes of 1
+	spike_gen = SpikeGenerator(shape=???, spike_prob=???)
+	dense_input = Dense(weights=???) # TODO: does this control which 10 neurons spike or the spikegen?
+	spike_gen.s_out.connect(dense_input.s_in)
+	dense_input.a_out.connect(lif.a_in)
 
-spike_gen = SpikeGenerator(shape=???, spike_prob=???)
-dense_input = Dense(weights=???)
-spike_get.s_out.connect(dense_input.s_in)
-dense_input.a_out.connect(lif.a_in)
+	run_condition = RunSteps(num_steps=num_steps)
+	run_cfg = Loihi1SimCfg()
+	lif.run(condition=run_condition, run_cfg=run_cfg)
 
-run_condition = RunSteps(num_steps=num_steps)
-run_cfg = Loihi1SimCfg()
-lif.run(condition=run_condition, run_cfg=run_cfg)
+	data_lif = monitor_lif.get_data()
 
-data_lif = monitor_lif.get_data()
+	lif.stop()
 
-lif.stop()
+	return load_time, sim_time, monitor_time
+
+
+if __name__ == '__main__':
+    N_list = [100, 200, 500, 1000]
+    d_list = [0.1, 0.3, 0.5, 1.0]
+    num_steps = 1000
+
+    num_evaluations = 5
+    cap_time = ???
+
+    for N in N_list:
+    	for d in d_list:
+
+    		print("Beginning experiment N =", N, "d =", d)
+    		loads = []
+    		sims = []
+    		monitors = []
+
+    		for i in num_evaluations:
+    			# TODO: figure out how to continue if 15 min have elapsed
+    			# --> might be easier to just make bash script that does this
+    			load, sim, monitor = random(N, d, num_steps)
+    			loads.append(load)
+    			sims.append(sim)
+    			monitors.append(monitor)
+
+    			print("Evaluation", i)
+    			# print("load", load)
+    			# print("sim", sim)
+    			# print("monitor", monitor)
+
+    		avg_load = loads.mean() # TODO
+    		avg_sim = sims.mean()
+    		avg_monitor = monitors.mean()
+
+    		print("Completed experiment N =", N, "d = ", d)
+    		print("loads", loads)
+			print("sims", sims)
+			print("monitors", monitors)
+
+			print("avg_load, avg_sim, avg_monitor")
+			print(avg_load, avg_sim, avg_monitor)
+
 
 
--- benchmark/shortestpath.py
+++ benchmark/shortestpath.py
@@ -26,5 +26,74 @@ keep running bucket of node_ids, every outgoing edge from a node puts another
 	tokens for m+1 node)
 for rest of n nodes, draw m bucket tokens and add edge to each unique node_id
 
---> make directed by following above, then randomly choosing direction of each edge
+--> make undirected by following above, then randomly choose direction of each edge
 '''
+
+def shortest_path(size: int):
+	time = 0
+
+	# Graph generation
+	n = size
+	m = n / 5 # TODO
+
+	graph = ??? # TODO: some numpy n-d array
+
+	# TODO: add edges from node m to nodes [0, m-1] in the graph (both ways)
+
+	# TODO: all this syntax might suck
+	bucket = [i for i in range(m)]
+	bucket.extend([m for i in range(m)])
+
+	for node in range(m+1, n):
+		# shuffle bucket
+		edges = bucket[:m]
+		# add edges to graph (both ways)
+		bucket.extend(edges)
+
+
+	lif = LIF(shape=???, vth=, dv=, du=, bias_mant=, name="LIF_neurons")
+	dense = Dense(weights=???, name="dense")
+
+	# recurrent connection
+	lif.s_out.connect(dense.s_in)
+	dense.a_out.connect(lif.a_in)
+
+	# TODO: this just needs spikes of 1
+	spike_gen = SpikeGenerator(shape=???, spike_prob=???)
+	dense_input = Dense(weights=???) # TODO: one node should be spiked
+	spike_gen.s_out.connect(dense_input.s_in)
+	dense_input.a_out.connect(lif.a_in)
+
+	# TODO: the run condition is continuous? how do we know when all neurons have been spiked?
+	# --> max number of steps equal to total num edges
+	
+	# run_condition = RunSteps(num_steps=num_steps)
+	# run_cfg = Loihi1SimCfg()
+	# lif.run(condition=run_condition, run_cfg=run_cfg)
+
+
+	lif.stop()
+
+	return time
+
+
+if __name__ == '__main__':
+	sizes = [500, 5000, 10000]
+
+	num_evaluations = 5
+
+	for size in sizes:
+		print("Beginning experiment size =", size)
+		times = []
+
+		for i in num_evaluations:
+			time = shortest_path(size)
+			times.append(time)
+			print("Evaluation", i)
+
+		avg_time = times.mean()
+
+		print("Completed experiment size =", size)
+		print("times", times)
+		print("avg_time")
+		print(avg_time)
